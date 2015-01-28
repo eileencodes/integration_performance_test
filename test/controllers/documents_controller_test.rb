89a4c92a9368d1246af9f46ac38d0d4a3df06bea
eileencodes <eileencodes@gmail.com>
2015-01-28 10:07:38 -0500

    Add HtmlGraphPrinter and comment each test
    For easier running each item is commented with how to run it. But I'm
    now realizing I'm not profiling minitest, just Rails so creating new
    files to run around each minitest test.

--- test/controllers/documents_controller_test.rb
+++ test/controllers/documents_controller_test.rb
@@ -1,27 +1,37 @@
 require 'test_helper'
 
 class DocumentsControllerTest < ActionController::TestCase
+  # standard test
   test "index" do
     get :index
     assert_equal 200, response.status
   end
 
+  # will create a call stack and html graph output
+  # ruby -I lib:test test/controllers/documents_controller_test.rb -n test_index_rp
   test "index rp" do
     result = RubyProf.profile do
       get :index
       assert_equal 200, response.status
     end
-    printer = RubyProf::CallStackPrinter.new(result)
-    printer.print(STDOUT, {})
+    File.open('graphs/inside_minitest/index_controller_callstack.html', 'w') do |file|
+      RubyProf::CallStackPrinter.new(result).print(file)
+    end
+
+    File.open('graphs/inside_minitest/index_controller_htmlgraph.html', 'w') do |file|
+      RubyProf::GraphHtmlPrinter.new(result).print(file)
+    end
   end
 
+  # ruby -I lib:test test/controllers/documents_controller_test.rb -n test_index_flame
   test "index flame" do
-    Flamegraph.generate("graphs/#{Time.now}_controller_index.html") do
+    Flamegraph.generate("graphs/inside_minitest/index_controller_flamegraph.html") do
       get :index
       assert_equal 200, response.status
     end
   end
 
+  # standard test
   test "create" do
     post :create, document: { title: "New things", content: "Doing them" }
 
@@ -30,6 +40,7 @@ class DocumentsControllerTest < ActionController::TestCase
     assert_equal 'Doing them', document.content
   end
 
+  # ruby -I lib:test test/controllers/documents_controller_test.rb -n test_create_rp
   test "create rp" do
     result = RubyProf.profile do
       post :create, document: { title: "New things", content: "Doing them" }
@@ -38,12 +49,18 @@ class DocumentsControllerTest < ActionController::TestCase
       assert_equal 'New things', document.title
       assert_equal 'Doing them', document.content
     end
-    printer = RubyProf::CallStackPrinter.new(result)
-    printer.print(STDOUT, {})
+    File.open('graphs/inside_minitest/create_controller_callstack.html', 'w') do |file|
+      RubyProf::CallStackPrinter.new(result).print(file)
+    end
+
+    File.open('graphs/inside_minitest/create_controller_htmlgraph.html', 'w') do |file|
+      RubyProf::GraphHtmlPrinter.new(result).print(file)
+    end
   end
 
+  # ruby -I lib:test test/controllers/documents_controller_test.rb -n test_create_flame
   test "create flame" do
-    Flamegraph.generate("graphs/#{Time.now}_controller_create.html") do
+    Flamegraph.generate("graphs/inside_minitest/create_controller_flamegraph.html") do
       post :create, document: { title: "New things", content: "Doing them" }
 
       document = Document.last
--- test/integration/documents_integration_test.rb
+++ test/integration/documents_integration_test.rb
@@ -1,27 +1,36 @@
 require 'test_helper'
 
 class DocumentsIntegrationTest < ActionDispatch::IntegrationTest
+  # standard test
   test "index" do
     get '/documents'
     assert_equal 200, response.status
   end
 
+  # ruby -I lib:test test/integration/documents_integration_test.rb -n test_index_rp
   test "index rp" do
     result = RubyProf.profile do
       get '/documents'
       assert_equal 200, response.status
     end
-    printer = RubyProf::CallStackPrinter.new(result)
-    printer.print(STDOUT, {})
+    File.open('graphs/inside_minitest/index_integration_callstack.html', 'w') do |file|
+      RubyProf::CallStackPrinter.new(result).print(file)
+    end
+
+    File.open('graphs/inside_minitest/index_integration_htmlgraph.html', 'w') do |file|
+      RubyProf::GraphHtmlPrinter.new(result).print(file)
+    end
   end
 
+  # ruby -I lib:test test/integration/documents_integration_test.rb -n test_index_flame
   test "index flame" do
-    Flamegraph.generate("graphs/#{Time.now}_integration_index.html") do
+    Flamegraph.generate("graphs/inside_minitest/index_integration_flamegraph.html") do
       get '/documents'
       assert_equal 200, response.status
     end
   end
 
+  # standard test
   test "create" do
     post '/documents', document: { title: "New things", content: "Doing them" }
 
@@ -30,6 +39,7 @@ class DocumentsIntegrationTest < ActionDispatch::IntegrationTest
     assert_equal 'Doing them', document.content
   end
 
+  # ruby -I lib:test test/integration/documents_integration_test.rb -n test_create_rp
   test "create rp" do
     result = RubyProf.profile do
       post '/documents', document: { title: "New things", content: "Doing them" }
@@ -38,12 +48,18 @@ class DocumentsIntegrationTest < ActionDispatch::IntegrationTest
       assert_equal 'New things', document.title
       assert_equal 'Doing them', document.content
     end
-    printer = RubyProf::CallStackPrinter.new(result)
-    printer.print(STDOUT, {})
+    File.open('graphs/inside_minitest/create_integration_callstack.html', 'w') do |file|
+      RubyProf::CallStackPrinter.new(result).print(file)
+    end
+
+    File.open('graphs/inside_minitest/create_integration_htmlgraph.html', 'w') do |file|
+      RubyProf::GraphHtmlPrinter.new(result).print(file)
+    end
   end
 
+  # ruby -I lib:test test/integration/documents_integration_test.rb -n test_create_flame
   test "create flame" do
-    Flamegraph.generate("graphs/#{Time.now}_integration_create.html") do
+    Flamegraph.generate("graphs/inside_minitest/create_integration_flamegraph.html") do
       post '/documents', document: { title: "New things", content: "Doing them" }
 
       document = Document.last
