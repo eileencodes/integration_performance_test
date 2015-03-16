b391b20a554ba6c4b2e89fdca7092a990a63624b
eileencodes <eileencodes@gmail.com>
2015-03-16 18:26:08 -0400

    Cleanup test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -12,8 +12,7 @@ Rails.application.configure do
   # preloads Rails for running tests, you may have to set it to true.
   config.eager_load = false
 
-  config.logger = Logger.new(nil)
-  config.log_level = :FATAL
+  config.log_level = :fatal
   # Configure static file server for tests with Cache-Control for performance.
   config.serve_static_files   = true
   config.static_cache_control = 'public, max-age=3600'
